a48c2ee27d0563686c8fc2bada803f506d104b20
ApostolOleg <apostololeg0@gmail.com>
2026-03-20 18:55:32 +0200

    refactor: reorganize Header and Footer components, update import paths (#37)

--- src/app/layouts/Header.jsx
+++ src/app/layouts/Header.jsx
@@ -1,9 +1,9 @@
-import logoImage from "../shared/assets/logo-icon.png";
+import logoImage from "../../shared/assets/logo-icon.png";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../features/auth/hooks/useAuth.js";
+import { useAuth } from "../../features/auth/hooks/useAuth.js";
 import { useState } from "react";
-import ModalConfirm from "../shared/ui/ModalConfirm.jsx";
-import Avatar from "../shared/ui/Avatar.jsx";
+import ModalConfirm from "../../shared/ui/ModalConfirm.jsx";
+import Avatar from "../../shared/ui/Avatar.jsx";
 
 export default function Header() {
 	const { user, logout } = useAuth();
